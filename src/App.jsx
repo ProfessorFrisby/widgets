3b73af4be76c80d2c70580d48ec0babf22c15640
Potro <potro@getnea.com>
2021-01-25 19:01:43 +0000

    initial repo

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { h } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
+import * as R from 'ramda';
+import crocks from 'crocks';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  const greet = R.replace('{name}', R.__, 'Hello, {name}');
+  
+  console.log(crocks.safe);
+  console.log(greet('NEA'));
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit! <code>src/App.jsx</code> and save to reload.
+        </p>
+        <p>
+          Page has been open for <code>{count}</code> seconds.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://preactjs.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Preact
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
